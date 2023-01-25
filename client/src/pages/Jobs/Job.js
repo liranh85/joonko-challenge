bd3a6617d8b2622b76eb499d61013ce35e874660
Liran Harary <liranh85@gmail.com>
2023-01-25 16:54:27 +0200

    Jobs page: WIP 2

--- client/src/pages/Jobs/Job.js
+++ client/src/pages/Jobs/Job.js
@@ -13,10 +13,16 @@ const Job = ({
   return (
     <div className="job">
       <div className="job-header" onClick={onClickHeader}>
-        <h2 className="job-name">
-          <span>{name}</span>
-          <span>&#8964;</span>
-        </h2>
+        <div>
+          <h2 className="job-name">
+            <span>{name}</span>
+            <span>&#8964;</span>
+          </h2>
+          <div className="job-main-details">
+            <h3>{location}</h3>
+            <h3>{department}</h3>
+          </div>
+        </div>
         <a href={applyUrl} target="_blank" rel="noreferrer">
           Apply
         </a>
